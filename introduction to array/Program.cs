27dcdfeb6ad7d928f54d44e2e995ccb33c4d4add
User <User@LAPTOP-B0ASERR3>
2026-01-08 16:14:32 +0500

    Add project files.

---
+++ introduction to array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introduction_to_array
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+                //1. Array Declaration and Initialization
+                int[] numbers = { 10, 20, 30, 40, 50 };
+
+              Console.WriteLine(numbers[2]);
+            //now we use loops to access arrays
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+                //the next one is foreach loop
+                foreach (int item in numbers)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+    }
+
---
+++ introduction to array/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("introduction to array")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("introduction to array")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d22f9ddb-cb52-4fd9-8c78-10ac9a033fc0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
